f0d312207910992133dfe9ff8b94748d41c27e76
iDarkGG <harryriva14@gmail.com>
2023-03-22 18:49:00 -0600

    XmlDataBase Handling

--- AppDEBUGGER/Program.cs
+++ AppDEBUGGER/Program.cs
@@ -1,35 +1,124 @@
-﻿using System.Xml;
+﻿using System.Runtime.CompilerServices;
+using System.Xml;
 using AppDependencies;
 public class uwu
 {
     static void Main(string[] args)
     {
-        
 
-        // Create a new XmlDocument
-        XmlDocument doc = new XmlDocument();
 
-        // Create the root element
-        XmlElement raiz = doc.CreateElement("Fields");
-        doc.AppendChild(raiz);
+        //// Create a new XmlDocument
+        //XmlDocument doc = new XmlDocument();
 
-        // Create the child elements
-        for (int i = 1; i <= 8; i++)
+        //// Create the root element
+        //XmlElement raiz = doc.CreateElement("Fields");
+        //doc.AppendChild(raiz);
+
+        //// Create the child elements
+        //for (int i = 1; i <= 8; i++)
+        //{
+        //    XmlElement field = doc.CreateElement("Field");
+        //    field.SetAttribute("id", i.ToString());
+
+        //    XmlElement value = doc.CreateElement("Value");
+        //    value.InnerText = "String " + i.ToString();
+
+        //    field.AppendChild(value);
+        //    raiz.AppendChild(field);
+        //}
+
+        //// Save the document to a file
+        //doc.Save("fields.xml");
+
+        //// Print the XML document to the console
+        //Console.WriteLine(doc.OuterXml);
+        void CrearBasedeDatos(string Name)
         {
-            XmlElement field = doc.CreateElement("Field");
-            field.SetAttribute("id", i.ToString());
+            XmlDocument DataBase = new XmlDocument();
+            XmlNode root = DataBase.CreateElement("DATOS");
+            DataBase.AppendChild(root);
 
-            XmlElement value = doc.CreateElement("Value");
-            value.InnerText = "String " + i.ToString();
 
-            field.AppendChild(value);
-            raiz.AppendChild(field);
+            XmlElement UserName = DataBase.CreateElement("Default_UserName");
+            UserName.SetAttribute("value", "Admin");
+
+            XmlElement Password = DataBase.CreateElement("Default_Password");
+            Password.InnerText = "Admin123";
+
+            XmlElement Email = DataBase.CreateElement("Default_Email");
+            Email.InnerText = "Admin@gmail.com";
+
+            UserName.AppendChild(Password);
+            UserName.AppendChild(Email);
+
+            root.AppendChild(UserName);
+
+            DataBase.Save("Prueba2.xml");
+            DataBase.Save(Name + ".xml");
+
         }
 
-        // Save the document to a file
-        doc.Save("fields.xml");
+        void Search(string elementName, string attributeName, string attributeValue)
+        {
+            XmlDocument DataBase = new XmlDocument();
+            DataBase.Load("Prueba2.xml");
 
-        // Print the XML document to the console
-        Console.WriteLine(doc.OuterXml);
+            XmlNodeList nodes = DataBase.GetElementsByTagName(elementName);
+            foreach (XmlNode node in nodes)
+            {
+                if (node.Attributes[attributeName] != null && node.Attributes[attributeName].Value == attributeValue)
+                {
+                    Console.WriteLine("Element found!");
+                    Console.WriteLine(node.OuterXml);
+                    return;
+                }
+                else
+                {
+                    XmlNode childNode = node.FirstChild;
+
+                    while (childNode != null)
+                    {
+                        if (childNode.Attributes != null && childNode.Attributes[attributeName] != null && childNode.Attributes[attributeName].Value == attributeValue)
+                        {
+                            Console.WriteLine("Element found!");
+                            Console.WriteLine(childNode.OuterXml);
+                            return;
+                        }
+                        else
+                        {
+                            Search2(childNode, attributeName, attributeValue);
+                        }
+                        childNode = childNode.NextSibling;
+                    }
+                }
+            }
+
+            Console.WriteLine("Element not found.");
+        }
+
+        void Search2(XmlNode node, string attributeName, string attributeValue)
+        {
+            XmlNode childNode = node.FirstChild;
+            while (childNode != null)
+            {
+                if (childNode.Attributes != null && childNode.Attributes[attributeName] != null && childNode.Attributes[attributeName].Value == attributeValue)
+                {
+                    Console.WriteLine("Element found!");
+                    Console.WriteLine(childNode.OuterXml);
+                    return;
+                }
+                else
+                {
+                    Search2(childNode, attributeName, attributeValue);
+                }
+                childNode = childNode.NextSibling;
+            }
+        }
+
+        string name = "prueba";
+
+        Search("DATOS", "value", "Admin");
+
+        //CrearBasedeDatos(name);
     }
 }
--- AppDependencies/FileParser.cs
+++ AppDependencies/FileParser.cs
@@ -11,19 +11,111 @@ namespace AppDependencies
     public class FileParser
     {
 
-        
-        public void CreateFile()
+
+        public void CrearBasedeDatos(string Name)
         {
-            XmlDocument documento = new XmlDocument();
+            XmlDocument DataBase = new XmlDocument();
+            XmlNode root = DataBase.CreateElement("DATOS");
+            DataBase.AppendChild(root);
 
-            XmlElement raiz = documento.CreateElement("DataBase");
-            documento.AppendChild(raiz);
 
-            CurrentDataBuffer a = new CurrentDataBuffer();
+            XmlElement UserName = DataBase.CreateElement("Default_UserName");
+            UserName.SetAttribute("value", "Admin");
 
-            UserDataScheme var = a.dataReader();
-            
+            XmlElement Password = DataBase.CreateElement("Default_Password");
+            Password.InnerText = "Admin123";
 
+            XmlElement Email = DataBase.CreateElement("Default_Email");
+            Email.InnerText = "Admin@gmail.com";
+
+            UserName.AppendChild(Password);
+            UserName.AppendChild(Email);
+
+            root.AppendChild(UserName);
+
+            DataBase.Save("Prueba2.xml");
+            DataBase.Save(Name + ".xml");
+
+        }
+
+        public void Search(string elementName, string attributeName, string attributeValue)
+        {
+            XmlDocument DataBase = new XmlDocument();
+            DataBase.Load("Prueba2.xml");
+
+            XmlNodeList nodes = DataBase.GetElementsByTagName(elementName);
+            foreach (XmlNode node in nodes)
+            {
+                if (node.Attributes[attributeName] != null && node.Attributes[attributeName].Value == attributeValue)
+                {
+                    Console.WriteLine("Elemento Encontrado");
+                    Console.WriteLine(node.OuterXml);
+                    return;
+                }
+                else
+                {
+                    XmlNode childNode = node.FirstChild;
+
+                    while (childNode != null)
+                    {
+                        if (childNode.Attributes != null && childNode.Attributes[attributeName] != null && childNode.Attributes[attributeName].Value == attributeValue)
+                        {
+                            Console.WriteLine("Elemento Encontrado");
+                            Console.WriteLine(childNode.OuterXml);
+                            return;
+                        }
+                        else
+                        {
+                            Search(childNode, attributeName, attributeValue);
+                        }
+                        childNode = childNode.NextSibling;
+                    }
+                }
+            }
+
+            Console.WriteLine("Elemento no Encontrado");
+        }
+
+        private void Search(XmlNode nodo, string attributeName, string attributeValue)
+        {
+            XmlNode childNode = nodo.FirstChild;
+            while (childNode != null)
+            {
+                if (childNode.Attributes != null && childNode.Attributes[attributeName] != null && childNode.Attributes[attributeName].Value == attributeValue)
+                {
+                    Console.WriteLine("Elemento Encontrado");
+                    Console.WriteLine(childNode.OuterXml);
+                    return;
+                }
+                else
+                {
+                    Search(childNode, attributeName, attributeValue);
+                }
+                childNode = childNode.NextSibling;
+            }
+        }
+
+
+        public void AddData(string userName, string password, string email)
+        {
+            XmlDocument DataBase = new XmlDocument();
+            DataBase.Load("Prueba2.xml");
+
+
+            XmlElement UserName = DataBase.CreateElement("UserName");
+            UserName.SetAttribute("value", userName);
+
+            XmlElement Password = DataBase.CreateElement("Password");
+            Password.InnerText = password;
+
+            XmlElement Email = DataBase.CreateElement("Email");
+            Email.InnerText = email;
+
+            UserName.AppendChild(Password);
+            UserName.AppendChild(Email);
+
+            XmlNode nodoadd = DataBase.SelectSingleNode("/DATOS");
+            nodoadd.AppendChild(UserName);
         }
 
     }
